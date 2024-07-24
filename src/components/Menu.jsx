d693cfda3dd205d9431d5edb6d5d711d59ae91a5
Souparno <souparna2000shee@gmail.com>
2024-07-24 21:46:26 +0530

    single change

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -19,7 +19,10 @@ const Menu = ({ setMenu }) => {
                 <Link to={"/about"} className='px-4 py-2 hover:border-black border-[1px] border-solid rounded-md font-semibold' onClick={() => setMenu(false)}>About</Link>
                 <Link to={"/pricing"} className='px-4 py-2 hover:border-black border-[1px] border-solid rounded-md font-semibold' onClick={() => setMenu(false)}>Pricing</Link>
                 <Link to={"/contact"} className='px-4 py-2 hover:border-black border-[1px] border-solid rounded-md font-semibold' onClick={() => setMenu(false)}>Contact</Link>
-
+                <Link to={"/neoxedu"} className='px-4 py-2 hover:border-black border-[1px] border-solid rounded-md font-semibold relative' onClick={() => setMenu(false)}>
+                    Neox Edu
+                    <span className='ml-2 bg-[red] text-white p-1 rounded-3xl '>new</span>
+                </Link>
 
             </div>
             <Link className='bg-[#353535] px-6 py-3 rounded-xl text-white font-[700]'>Get In Touch</Link>
